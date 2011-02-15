d35389bef4ceaf0713697e09488e2b31c79d66ae
John O'Hara <johara@gmail.com>
2011-02-15 16:26:13 -0500

    Working solution to Problem 016

---
+++ src/com/github/johnnyo/euler/Problem016.java
@@ -0,0 +1,24 @@
+package com.github.johnnyo.euler;
+
+import java.math.BigInteger;
+
+public class Problem016 extends BaseTestCase {
+
+	@Override
+	public String getAnswer() {
+		return "1366";
+	}
+
+	@Override
+	public String solve() {
+		BigInteger big = new BigInteger("2");
+		big = big.pow(1000);
+		String s = big.toString();
+		int sum = 0;
+		for (char c : s.toCharArray()) {
+			sum = sum + Integer.parseInt("" + c);
+		}
+		return Integer.toString(sum);
+	}
+
+}
